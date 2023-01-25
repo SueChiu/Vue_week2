f4347af7e815551de1c228553918907233f8fb11
SueChiu <95234985+SueChiu@users.noreply.github.com>
2023-01-25 11:27:42 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -19,20 +19,8 @@ createApp({
         location.assign("products.html");
       })
       .catch(err => {
-        console.error(err);
         alert("登入失敗");
       })
     }
-  },
-  mounted() {
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)user\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-    axios.defaults.headers.common['Authorization'] = token; //每次(default)發出請求時，header加入此參數
-    axios.post(`${this.apiUrl}api/user/check`)
-    .then((res) => {
-      location.assign("products.html");
-    })
-    .catch((err) => {
-      console.log(err.message);
-    })
   }
 }).mount("#app");
